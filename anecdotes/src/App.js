db018360e2621ba35cbeb55e79c17c6523b29cb0
hitesh-chundi <hitesh.chundi@hotmail.com>
2023-08-23 16:41:58 +0530

    Solution to exercise 1.13

--- anecdotes/src/App.js
+++ anecdotes/src/App.js
@@ -12,27 +12,24 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
    
-  const [selected, setSelected] = useState(anecdotes[0])
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
-  const handleAnecdotes = () => setSelected(anecdotes[Math.floor(Math.random() * anecdotes.length)]);
+
+  const handleAnecdote = () => setSelected([Math.floor(Math.random() * anecdotes.length)]);
+  const handleVotes = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+  }
 
   return (
     <div>
-      <Header value={selected} />
-      <Button handleClick={handleAnecdotes} text="Next Anecdote" />
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVotes}>vote</button>
+      <button onClick={handleAnecdote}>next anecdote</button>
     </div>
-  )
-}
-
-const Header = (props) => {
-  return (
-    <h1>{props.value}</h1>
-  )
-}
-
-const Button = (props) => {
-  return (
-    <button onClick={props.handleClick}>{props.text}</button>
   )
 }
 
